69bbbd37d1a3e2443fd0ba17d502a286814eb4f0
ai4declic-sn <164817275+ai4declic-sn@users.noreply.github.com>
2024-03-24 19:01:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ shap_values = explainer.shap_values(X)
 st.header('Feature Importance')
 #plt.title('Feature importance based on SHAP values')
 #shap.summary_plot(shap_values, X)
-st.pyplot(bbox_inches='tight')
+#st.pyplot(bbox_inches='tight')
 #st.write('---')
 
 plt.title('Feature importance based on SHAP values (Bar)')
